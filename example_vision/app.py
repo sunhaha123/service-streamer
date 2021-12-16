1d339cdd2621b5446c5166b9510bb18bfef6675c
sunhaha123 <sunjh007@163.com>
2021-12-16 15:16:59 +0800

    Update app.py

--- example_vision/app.py
+++ example_vision/app.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 # Created by Meteorix at 2019/8/9
 from flask import Flask, jsonify, request
-from model import get_prediction, batch_prediction
+from example_vision.model import get_prediction,batch_prediction
 from service_streamer import ThreadedStreamer
-
+from service_streamer import ManagedModel, Streamer
 
 app = Flask(__name__)
 streamer = ThreadedStreamer(batch_prediction, batch_size=64)
@@ -23,7 +23,7 @@ def stream_predict():
     if request.method == 'POST':
         file = request.files['file']
         img_bytes = file.read()
-        class_id, class_name = streamer.predict([img_bytes])[0]
+        class_id, class_name = streamer.predict([img_bytes]*6)[0]
         return jsonify({'class_id': class_id, 'class_name': class_name})
 
 
