4e8768c1cb781e7413f8e51743b047ad798bbb17
sunhaha123 <sunjh007@163.com>
2021-12-17 11:22:11 +0800

    probality_vector

--- example_vision/model.py
+++ example_vision/model.py
@@ -44,12 +44,38 @@ def batch_prediction(image_bytes_batch):
     predicted_ids = y_hat.tolist()
     return [imagenet_class_index[str(i)] for i in predicted_ids]
 
+def get_probality(image_bytes):
+    try:
+        tensor = transform_image(image_bytes=image_bytes).to(device)
+        outputs = torch.softmax(model.forward(tensor).squeeze(),dim=0)
+        prediction = outputs.detach().cpu().numpy()
+        template = "class:{:<15} probability:{:.3f}"
+        index_pre = [(imagenet_class_index[str(index)],float(p)) for index, p in enumerate(prediction)]
+        #sort probality
+        index_pre.sort(key=lambda x:x[1],reverse = True)
+        text = [template.format(k[1],v) for k,v in index_pre]
+        return_info =  {'result':text}
+    except Exception as e:
+        return_info =  {"result": [str(e)]}
+    return return_info
+
+def get_vector(image_bytes):
+    try:
+        tensor = transform_image(image_bytes=image_bytes).to(device)
+        outputs = torch.softmax(model.extractor(tensor).squeeze(),dim=0)
+        prediction = outputs.detach().cpu().numpy()
+        return_info =  {'result':prediction.tolist()}
+    except Exception as e:
+        return_info =  {"result": [str(e)]}
+    return return_info
+
 
 if __name__ == "__main__":
     with open(r"cat.jpg", 'rb') as f:
         image_bytes = f.read()
 
-    result = get_prediction(image_bytes)
+    result = get_vector(image_bytes)
     print(result)
-    batch_result = batch_prediction([image_bytes] * 64)
-    assert batch_result == [result] * 64
+    # batch_result = batch_prediction([image_bytes])
+    # assert batch_result == [result] * 64
+    # print(batch_result)
